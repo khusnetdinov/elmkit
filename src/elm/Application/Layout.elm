f3c1f4ba0c4a2c646ccd39835163ffb2a2b1b241
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-10-04 17:46:12 +0300

    Hello world page

--- src/elm/Application/Layout.elm
+++ src/elm/Application/Layout.elm
@@ -1,8 +1,11 @@
 module Application.Layout exposing (view)
 
-import Html exposing (div, text)
-import Html.Attributes exposing (class)
+import Html exposing (div, img, h2, p, ul, li, a, text)
+import Html.Attributes exposing (class, src, alt, href)
 
 view =
-  div [class "e-application-layout"]
-      [ text "Application Layout" ]
+  div [ class "e-application-layout" ]
+    [ img [ class "elm-logo", src "/images/elm.png", alt "Elm boilerplate kit" ][]
+    , h2 [ class "title" ] [ text "Hello world from Elm boilerplate kit!" ]
+    ]
+
